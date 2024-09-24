f6621b710b46b3b69de1da2a4e6b830eee1970bc
杨涛 <2749552387@qq.com>
2024-09-24 15:05:36 +0800

    fix:修改readme.md

--- src/components/Trend.vue
+++ src/components/Trend.vue
@@ -2,7 +2,7 @@
  * @Author: 杨涛 2749552387@qq.com
  * @Date: 2024-09-24 11:31:46
  * @LastEditors: 杨涛 2749552387@qq.com
- * @LastEditTime: 2024-09-24 15:01:42
+ * @LastEditTime: 2024-09-24 15:04:07
  * @FilePath: \Midas-Insight\src\components\Trend.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
