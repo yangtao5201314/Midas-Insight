f27eeb4f18aa5dcf72d92653206ccabc54470ace
杨涛 <2749552387@qq.com>
2024-09-25 09:22:35 +0800

    fix:修改readme.md

--- src/components/Trend.vue
+++ src/components/Trend.vue
@@ -2,7 +2,7 @@
  * @Author: 杨涛 2749552387@qq.com
  * @Date: 2024-09-24 11:31:46
  * @LastEditors: 杨涛 2749552387@qq.com
- * @LastEditTime: 2024-09-24 17:37:05
+ * @LastEditTime: 2024-09-25 09:20:40
  * @FilePath: \Midas-Insight\src\components\Trend.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -17,7 +17,7 @@ const handleChange = (val: string[]) => {
   console.log(val);
 };
 // 日期范围选择
-const value1 = ref('2021-10-29')
+const value1 = ref('2024-1-1')
 const dataChaange = () => {
 
 }
@@ -54,6 +54,7 @@ onMounted(async () => {
 </template>
 
 <style scoped>
+
 .demo-collapse {
   padding-right: 20px;
 }
@@ -124,5 +125,105 @@ button:focus-visible {
   color: var(--el-text-color-secondary);
   font-size: 14px;
   margin-bottom: 20px;
+}
+
+/* 日期选择框的样式 */
+
+:deep(.el-date-picker).has-sidebar.has-time {
+    background: #04308D;
+    color: #fff;
+    border: 1px solid #326AFF
+}
+:deep(.el-date-picker__header-label) {
+    color: #ffffff;
+}
+:deep(.el-date-table) th {
+    color: #fff;
+}
+:deep(.el-icon-d-arrow-left:before) {
+    color: #fff;
+}
+.el-icon-arrow-left:before {
+    color: #fff;
+}
+.el-icon-arrow-right:before {
+    color: #fff;
+}
+.el-icon-d-arrow-right:before {
+    color: #fff;
+}
+:deep(.el-picker-panel__footer){
+    background-color: #04308D;
+    border: 1px solid #326AFF
+}
+:deep(.el-picker-panel [slot=sidebar]), .el-picker-panel__sidebar {
+    background-color: #04308D  !important;
+    border-right: 1px solid #326AFF;   
+}
+:deep(.el-picker-panel__shortcut) {
+    color: #fff;
+}
+:deep(.el-date-picker__time-header) {
+    border-bottom: 1px solid #326AFF;
+}
+.el-popper[x-placement^=bottom] .popper__arrow::after {
+    top: 1px;
+    margin-left: -6px;
+    border-top-width: 0;
+    border-bottom-color: #04308D;
+}
+.el-popper[x-placement^=top] .popper__arrow::after {
+    bottom: 1px;
+    margin-left: -6px;
+    border-top-color: #326AFF  !important;
+    border-bottom-width: 0;
+}
+:deep(.el-picker-panel) {
+    background: #04308D !important;
+    color: #fff;
+}
+:deep(.el-date-table td.in-range div, .el-date-table td.in-range div:hover, .el-date-table.is-week-mode .el-date-table__row.current div, .el-date-table.is-week-mode .el-date-table__row:hover div) {
+    background-color: #326AFFc9 !important;
+}
+.el-date-range-picker__time-header{
+    border-bottom: 1px solid #326AFF;
+}
+.el-date-range-picker__content.is-left{
+    border-right: 1px solid #326AFF;
+}
+:deep(.el-popper).is-light .el-popper__arrow::before {
+    border: 1px solid #326AFF;
+    background: #04308D;
+    right: 0;
+}
+.el-popper.is-pure {
+    border:  1px solid #326AFF;
+}
+:deep(.el-input__wrapper ){
+    border: 1px solid #ffffff !important;
+    box-shadow: 0 0 0 0px #151515 inset;
+    background: #151515 !important;
+}
+:deep(.el-input__wrapper) .el-input__inner {
+    background: #151515 !important;
+    font-size: 14px;
+    font-weight: 400;
+    color: #FFFFFF;
+}
+:deep(.el-picker-panel__footer) .el-button.is-text {
+    color: #fff;
+    border: 0 solid transparent;
+    background-color: transparent;
+}
+:deep(.el-picker-panel__footer) .el-button{
+    background-color: #326aff;
+    border: 1px solid #326AFF;
+    color: #fff;
+}
+:deep(.el-button).is-text:not(.is-disabled):hover {
+    background-color: #326aff;
+}
+.in-range{
+    color: #326aff;
 }
 </style>
